e55f736ea754fa3a4a12d988567d640df6c65ca5
Tony Rivera <tonyrivera@users.noreply.github.com>
2021-02-08 21:16:26 -0800

    Create uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,18 @@
+##!/bin/zsh
+
+# Instructions:
+# chmod +x uninstall.sh
+# ./uninstall.sh
+
+# Requirements:
+# python3
+# pip
+# requirements.txt
+
+echo "Activating virtual environment"
+source $(pwd)/venv/bin/activate
+
+echo "Deactivating virtual environment"
+deactivate
+
+echo "Removing virtual environment located at '$(pwd)/venv'"
