f0ce241fbc109da416b90a86d98ce41a46bd39b1
Tony Rivera <tonyrivera@users.noreply.github.com>
2021-02-15 17:46:09 -0800

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -4,10 +4,5 @@
 # chmod +x uninstall.sh
 # ./uninstall.sh
 
-# Requirements:
-# python3
-# pip
-# requirements.txt
-
 echo "Removing virtual environment located at '$(pwd)/venv'"
 rm -fi $(pwd)/venv/
