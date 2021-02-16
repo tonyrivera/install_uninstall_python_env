925a4f05c4798e4810248199ffc64d85902093bc
Tony Rivera <tonyrivera@users.noreply.github.com>
2021-02-15 17:44:34 -0800

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -9,11 +9,5 @@
 # pip
 # requirements.txt
 
-echo "Activating virtual environment"
-source $(pwd)/venv/bin/activate
-
-echo "Deactivating virtual environment"
-deactivate
-
 echo "Removing virtual environment located at '$(pwd)/venv'"
 rm -fi $(pwd)/venv/
